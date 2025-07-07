abb41d71c0c2a9334bddab945bd4ca42decd6ec2
hendisantika <hendisantika@yahoo.co.id>
2025-07-08 05:55:50 +0700

    feat: Add ApiResult :one:🫸🌀✏️📗🐧🐳

--- src/main/kotlin/id/my/hendisantika/kotlinsandbox/ApiResult.kt
+++ src/main/kotlin/id/my/hendisantika/kotlinsandbox/ApiResult.kt
@@ -15,3 +15,5 @@ sealed class ApiResult<T> {
     data class Success<T>(val data: T) : ApiResult<T>()
     data class Error<T>(val message: String) : ApiResult<T>()
 }
+
+fun <T> ApiResult<T>.getOrNull(): T? = if (this is ApiResult.Success) data else null
