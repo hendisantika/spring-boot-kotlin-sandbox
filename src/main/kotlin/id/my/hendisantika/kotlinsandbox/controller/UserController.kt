6fad81e30e1b755c56336af212faa70e318300da
hendisantika <hendisantika@yahoo.co.id>
2025-07-08 06:09:47 +0700

    feat: Add UserController🫸🌀✏️📗🐧🐳

---
+++ src/main/kotlin/id/my/hendisantika/kotlinsandbox/controller/UserController.kt
@@ -0,0 +1,17 @@
+package id.my.hendisantika.kotlinsandbox.controller
+
+import org.springframework.web.bind.annotation.RestController
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : kotlin-sandbox
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 08/07/25
+ * Time: 06.09
+ * To change this template use File | Settings | File Templates.
+ */
+@RestController
+class UserController
