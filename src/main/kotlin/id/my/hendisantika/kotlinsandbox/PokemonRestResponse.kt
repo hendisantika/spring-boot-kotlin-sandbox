92e9ac14c9624deba4e8dca0af7f6e167f790c19
hendisantika <hendisantika@yahoo.co.id>
2025-07-08 05:57:53 +0700

    feat: Add PokemonRestResponse🫸🌀✏️📗🐧🐳

---
+++ src/main/kotlin/id/my/hendisantika/kotlinsandbox/PokemonRestResponse.kt
@@ -0,0 +1,22 @@
+package id.my.hendisantika.kotlinsandbox
+
+import kotlinx.serialization.Serializable
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : kotlin-sandbox
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 08/07/25
+ * Time: 05.57
+ * To change this template use File | Settings | File Templates.
+ */
+@Serializable
+data class PokemonRestResponse(
+    val id: Int,
+    val name: String,
+    val weight: Int,
+    val height: Int
+)
