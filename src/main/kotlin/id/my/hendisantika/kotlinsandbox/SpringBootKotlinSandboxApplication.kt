091c5b5d48f9d8b86549b3552bb441bf2269270b
hendisantika <hendisantika@yahoo.co.id>
2025-07-08 05:53:01 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳

---
+++ src/main/kotlin/id/my/hendisantika/kotlinsandbox/SpringBootKotlinSandboxApplication.kt
@@ -0,0 +1,11 @@
+package id.my.hendisantika.kotlinsandbox
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootKotlinSandboxApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootKotlinSandboxApplication>(*args)
+}
---
+++ src/test/kotlin/id/my/hendisantika/kotlinsandbox/SpringBootKotlinSandboxApplicationTests.kt
@@ -0,0 +1,13 @@
+package id.my.hendisantika.kotlinsandbox
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootKotlinSandboxApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
