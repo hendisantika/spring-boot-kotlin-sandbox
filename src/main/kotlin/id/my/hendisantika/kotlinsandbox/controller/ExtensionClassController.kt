dde3ec0799cb9bf0c424b839d512155cbfd3d005
hendisantika <hendisantika@yahoo.co.id>
2025-07-08 06:00:28 +0700

    feat: Add ExtensionClassController🫸🌀✏️📗🐧🐳

---
+++ src/main/kotlin/id/my/hendisantika/kotlinsandbox/controller/ExtensionClassController.kt
@@ -0,0 +1,19 @@
+package id.my.hendisantika.kotlinsandbox.controller
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : kotlin-sandbox
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 08/07/25
+ * Time: 06.00
+ * To change this template use File | Settings | File Templates.
+ */
+@RestController
+@RequestMapping("/extension_class")
+class ExtensionClassController
