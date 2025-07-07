e08f54ed92d4c2acd31bfdc6a7d7cce2e94b352c
hendisantika <hendisantika@yahoo.co.id>
2025-07-08 05:55:26 +0700

    feat: Add ApiResult Class🫸🌀✏️📗🐧🐳

---
+++ src/main/kotlin/id/my/hendisantika/kotlinsandbox/ApiResult.kt
@@ -0,0 +1,17 @@
+package id.my.hendisantika.kotlinsandbox
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : kotlin-sandbox
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 08/07/25
+ * Time: 05.55
+ * To change this template use File | Settings | File Templates.
+ */
+sealed class ApiResult<T> {
+    data class Success<T>(val data: T) : ApiResult<T>()
+    data class Error<T>(val message: String) : ApiResult<T>()
+}
