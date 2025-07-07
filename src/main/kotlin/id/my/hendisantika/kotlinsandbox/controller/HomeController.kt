9e83bbe4cd0e8816e954f7c9db16987c6bfec6e0
hendisantika <hendisantika@yahoo.co.id>
2025-07-08 06:02:59 +0700

    feat: Add HomeController🫸🌀✏️📗🐧🐳

---
+++ src/main/kotlin/id/my/hendisantika/kotlinsandbox/controller/HomeController.kt
@@ -0,0 +1,20 @@
+package id.my.hendisantika.kotlinsandbox.controller
+
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.web.bind.annotation.RestController
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : kotlin-sandbox
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 08/07/25
+ * Time: 06.02
+ * To change this template use File | Settings | File Templates.
+ */
+@RestController
+class HomeController(
+    private val kafkaTemplate: KafkaTemplate<String, String>
+)
